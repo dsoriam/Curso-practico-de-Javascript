1c64c73edafa49bc180b7fc8fa54906671b4ce6c
dsoriam <dsoriam@gmail.com>
2022-05-18 11:59:39 -0300

    Funciones

--- figuras.js
+++ figuras.js
@@ -52,6 +52,16 @@ function areaCirculo(radio){
     return (radio * radio) * PI;
 }
 
+function alturaIsosceles (lado1, lado2, base){
+    if (lado1 == lado2 && lado1 != base)
+    {
+        const altura = Math.sqrt(Math.pow(lado1, 2) - ((Math.pow(base, 2))/4) );
+        return altura
+    }
+    else{
+        return "No es isosceles"
+    }
+}
 
 
 console.groupEnd();
@@ -73,4 +83,51 @@ function calcularAreaCuadrado() {
     
     const area = areaCuadrado(value);
     alert(area);
+}
+
+//TRIANGULO
+
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("inputlado1");
+    const lado1 = input1.value;
+
+    const input2 = document.getElementById("inputlado2");
+    const lado2 = input2.value;
+
+    const input3 = document.getElementById("inputbase");
+    const base = input3.value;
+    
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+
+    const input3 = document.getElementById("inputbase");
+    const base = input3.value;
+
+    const input4 = document.getElementById("inputaltura");
+    const altura = input4.value;
+    
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
+
+
+
+
+
+function calcularAlturaIsosceles (){
+
+    const input1 = document.getElementById("inputlado1");
+    const lado1 = input1.value;
+
+    const input2 = document.getElementById("inputlado2");
+    const lado2 = input2.value;
+
+    const input3 = document.getElementById("inputbase");
+    const base = input3.value;
+
+    const AltIsoceles = alturaIsosceles(lado1, lado2, base);
+    alert(AltIsoceles);
 }
