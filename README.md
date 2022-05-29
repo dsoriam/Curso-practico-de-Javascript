17ca985ae20f33f10c857b0767ea366ed526100b
dsoriam <dsoriam@gmail.com>
2022-05-29 20:01:27 -0300

    Cambio en el readme

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Platzi
 -Implementar las formulas en Javascript
 -Crear funciones
 -integrar JS con HTML
+
+
+## Taller 2 Descuento y porcentajes
+
+-Definir las formulas
+-Implementar las formulas en Javascript
+-Crear funciones
+-integrar JS con HTML
+
+
+## Taller 3 Promedio, mediana y moda
+
+-Definir las formulas
+-Implementar las formulas en Javascript
+-Crear funciones
+-integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -22,7 +22,7 @@ console.groupEnd();
 console.group("Triangulo")
 
 function perimetroTriangulo (lado1 , lado2, base,){
-    return lado1 + lado2 + base;
+    return parseInt(lado1) + parseInt(lado2) + parseInt(base) ;
 } 
 
 function areaTriangulo(base, altura){
@@ -34,7 +34,7 @@ console.groupEnd();
 
 //Circulo
 
-console.group("Circulo")
+console.group("Circulo");
 
 
 const PI = Math.PI;
@@ -114,9 +114,6 @@ function calcularAreaTriangulo() {
 }
 
 
-
-
-
 function calcularAlturaIsosceles (){
 
     const input1 = document.getElementById("inputlado1");
@@ -131,3 +128,146 @@ function calcularAlturaIsosceles (){
     const AltIsoceles = alturaIsosceles(lado1, lado2, base);
     alert(AltIsoceles);
 }
+
+
+//DESCUENTO DE PRODUCTO
+
+function descuentoTotal (precio, descuento){
+    
+    if (precio > 0 && descuento >= 0)
+    {
+        const total = (precio*(100 - descuento)) / 100;
+        return total;
+    }
+    else
+    {
+        return "El precio y el descuento debe ser mayor a 0";
+    }
+}
+
+
+
+
+
+
+function calcularDescuento(){
+
+    const coupons = [
+        "promo1",
+        "amigo",
+        "cliente",
+    ];
+
+    const input1 = document.getElementById("InputPrecio");
+    const precio = input1.value;
+
+    const input2 = document.getElementById("InputDescuento");
+    const descuento = input2.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const cupon = inputCoupon.value;
+
+    let descuentot;
+
+    switch(cupon) {
+        case coupons[0]: // "promo1"
+          descuentot = parseInt(descuento)  + parseInt(15);
+        break;
+        case coupons[1]: // "amigo"
+          descuentot = parseInt(descuento)  + parseInt(25) ;
+        break;
+        case coupons[2]: // "cliente"
+          descuentot = parseInt(descuento)  + parseInt(30);
+        break;
+      }
+
+      if (!coupons.includes(cupon))
+         {
+        alert("El cupón " + cupon + "no es válido");
+         }
+
+    const desc = descuentoTotal(precio, descuentot);
+
+    const resultado = document.getElementById("resultadoP");
+    resultado.innerText = "El precio con descuento es: " + desc;
+}
+
+
+//multiplicar sin usar el asterisco
+function multiplicar (a, b){
+ 
+    
+
+    if (a> 0 || b > 0) 
+    {
+        a = Math.abs(a);
+        for (var x= 1; x <Math.abs(b); x+a)
+        {
+             a += a;
+            //alert (a);
+        }
+    }
+    alert(a);
+}
+
+
+
+//Promedio
+
+
+
+function calcularMediaArismetrica (lista){
+    
+  /*   let sumaLista = 0;
+    
+    for(let i = 0; i< lista.length; i++){
+        sumaLista = sumaLista + lista[i];
+    } */
+    
+    const sumaLista = lista.reduce(
+        function (valorAcumulado =0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        
+    }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+};
+
+
+const lista1 =[
+    100,
+    200,
+    300,
+    40000000
+];
+
+
+const mitadLista1 = parseInt(lista1.length /2);
+
+function esPar(numerito){
+    if(numerito % 2 === 0 ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let mediana;
+if (esPar(lista1.length)){
+    const elemento1=lista1[mitadLista1-1];
+    const elemento2=lista1[mitadLista1];
+    const promedioElemento1y2=calcularMediaArismetrica([elemento1,elemento2]);
+    mediana=promedioElemento1y2;
+    }
+    else{
+        mediana = (lista1 [mitadLista1]);
+    };
+
+
+
+
+
+
